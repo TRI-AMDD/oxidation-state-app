d9c7f0450e734216be2efe4ebae5c434c3d1e2ac
Danny <daniel.martinez@slalom.com>
2023-05-08 09:39:50 -0500

    test data for demo purposes

---
+++ ui/src/test-data/test-data.ts
@@ -0,0 +1,20 @@
+import { OxidationStatesTableItem } from '../models/DataViewerModel';
+
+export const dataTableTestData: OxidationStatesTableItem[] = [
+    {
+        id: 1,
+        oxidationState: '2Li1+Mn3+Mn4+O2-Li1+Mn3+Mn4+4O2-Li1+Mn3+Mn4+O2-',
+        likelihoodCurrentElecChemPotential: 0.49,
+        likelihoodOptimalElecChemPotential: 0.49,
+        optimalElecChemPotential: 3.142,
+        globalInstabilityIndex: 0.17
+    },
+    {
+        id: 2,
+        oxidationState: 'Li1+Mn3+Mn2+O1-',
+        likelihoodCurrentElecChemPotential: 0.01,
+        likelihoodOptimalElecChemPotential: 0.02,
+        optimalElecChemPotential: 3.872,
+        globalInstabilityIndex: 0.38
+    }
+];
