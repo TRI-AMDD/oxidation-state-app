9564c8db6b5bcbc0783c4b3ae8ce2e1aa629206e
Danny <daniel.martinez@slalom.com>
2023-05-08 09:54:00 -0500

    update for demo

--- ui/src/test-data/test-data.ts
+++ ui/src/test-data/test-data.ts
@@ -16,5 +16,29 @@ export const dataTableTestData: OxidationStatesTableItem[] = [
         likelihoodOptimalElecChemPotential: 0.02,
         optimalElecChemPotential: 3.872,
         globalInstabilityIndex: 0.38
+    },
+    {
+        id: 3,
+        oxidationState: '2Li1+Mn3+Mn4+O2-Li1+Mn3+Mn4+4O2-Li1+Mn3+Mn4+O2-',
+        likelihoodCurrentElecChemPotential: 0.49,
+        likelihoodOptimalElecChemPotential: 0.49,
+        optimalElecChemPotential: 3.142,
+        globalInstabilityIndex: 0.17
+    },
+    {
+        id: 4,
+        oxidationState: '2Li1+Mn3+Mn4+O2-Li1+Mn3+Mn4+4O2-Li1+Mn3+Mn4+O2-',
+        likelihoodCurrentElecChemPotential: 0.49,
+        likelihoodOptimalElecChemPotential: 0.49,
+        optimalElecChemPotential: 3.142,
+        globalInstabilityIndex: 0.17
+    },
+    {
+        id: 5,
+        oxidationState: '2Li1+Mn3+Mn4+O2-Li1+Mn3+Mn4+4O2-Li1+Mn3+Mn4+O2-',
+        likelihoodCurrentElecChemPotential: 0.49,
+        likelihoodOptimalElecChemPotential: 0.49,
+        optimalElecChemPotential: 3.142,
+        globalInstabilityIndex: 0.17
     }
 ];
