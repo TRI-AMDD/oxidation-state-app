a85c2b15249a53a3240d429418135db03c88aabf
Michael Puzon <michael.puzon@tri.global>
2023-05-22 18:47:53 -0700

    Draw graphs

--- ui/src/components/DataViewer/Graph/PlotlyGraph/PlotlyGraph.tsx
+++ ui/src/components/DataViewer/Graph/PlotlyGraph/PlotlyGraph.tsx
@@ -5,22 +5,46 @@ import testData from './test.json';
 import Plot from 'react-plotly.js';
 
 interface PlotData {
-    [key: number]: {
-        potential: number;
-        likelihood: number;
-    }[];
+    oxidationState: number;
+    potential: number[];
+    likelihood: number[];
+}
+
+interface GraphData {
+    name: string;
+    x: number[];
+    y: number[];
 }
 
 const PlotlyGraph = () => {
     const items = useMemo(() => {
-        const parsedData: PlotData = {};
-        const uniqueStates = [...new Set(testData.map((item) => item.oxidationState))];
+        const parsedData: PlotData[] = [];
+        const uniqueStates = [...new Set(testData.map((item) => item.oxidationState))].sort();
         console.log(uniqueStates);
         for (const unique of uniqueStates) {
-            parsedData[unique] = testData.filter((item) => item.oxidationState === unique);
+            parsedData.push({
+                oxidationState: unique,
+                potential: testData.filter((item) => item.oxidationState === unique).map((item) => item.potential),
+                likelihood: testData.filter((item) => item.oxidationState === unique).map((item) => item.likelihood)
+            });
         }
 
-        return parsedData;
+        const graphData: GraphData[] = [];
+
+        for (let i = 0; i < parsedData.length; i++) {
+            if (parsedData[i].oxidationState !== 0) {
+                const potentialPrevReverse = [...parsedData[i - 1].potential].reverse();
+                const likelihoodPrevReverse = [...parsedData[i - 1].likelihood].reverse();
+
+                graphData.push({
+                    name: parsedData[i].oxidationState.toString(),
+                    x: [...parsedData[i].potential, ...potentialPrevReverse],
+                    y: [...parsedData[i].likelihood, ...likelihoodPrevReverse]
+                });
+            }
+        }
+
+        return graphData;
     }, []);
 
     console.log(items);
@@ -31,12 +55,14 @@ const PlotlyGraph = () => {
                 {
                     // x: [1, 2, 3],
                     // y: [2, 6, 3],
-                    x: items[0].map((item) => item.potential),
-                    y: items[0].map((item) => item.likelihood),
+                    x: items[0].x,
+                    y: items[0].y,
+                    name: items[0].name,
+                    // y: items[0].map((item) => item.likelihood),
                     type: 'scatter',
                     fill: 'toself',
-                    mode: 'lines',
-                    name: '+1'
+                    mode: 'lines'
+
                     // mode: 'lines+markers',
                     // marker: { color: 'red' }
                 }
