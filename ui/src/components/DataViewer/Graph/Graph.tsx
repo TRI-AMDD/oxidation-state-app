23896573280aff90e58e189a70cfd387ae28e3dc
mike puzon <michael.puzon@tri.global>
2023-05-24 16:21:00 -0700

    Re-draw the graphs in canvas

--- ui/src/components/DataViewer/Graph/Graph.tsx
+++ ui/src/components/DataViewer/Graph/Graph.tsx
@@ -1,12 +1,13 @@
 import { useAtom } from 'jotai';
 import { oxidationDataAtom } from 'atoms/atoms';
+import CanvasGraph from './CanvasGraph/CanvasGraph';
 import styles from './Graph.module.css';
-import PlotlyGraph from './PlotlyGraph/PlotlyGraph';
+// import PlotlyGraph from './PlotlyGraph/PlotlyGraph';
 
 const Graph = () => {
     const [oxidationData] = useAtom(oxidationDataAtom);
 
-    return <div className={styles.container}>{oxidationData && <PlotlyGraph data={oxidationData} />}</div>;
+    return <div className={styles.container}>{oxidationData && <CanvasGraph data={oxidationData} />}</div>;
 };
 
 export default Graph;
---
+++ ui/src/constants/colors.tsx
@@ -0,0 +1,18 @@
+interface Palette {
+    [key: number]: string;
+}
+
+export const ColorPalette: Palette = {
+    [-1]: '#e0cee1',
+    [-2]: '#cd9fcc',
+    [-3]: '#c070c9',
+    [-4]: '#b63d9c',
+    1: '#bedbef',
+    2: '#7abdcd',
+    3: '#4f9bc5',
+    4: '#4a76b5',
+    5: '#4a51a0',
+    6: '#492c8a',
+    7: '#3a125b',
+    8: '#1a0b2b'
+};
