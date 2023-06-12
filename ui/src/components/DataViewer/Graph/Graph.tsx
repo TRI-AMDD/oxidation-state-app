30a38cf07006eea078dff606ad4f89efa88ed435
Danny <daniel.martinez@slalom.com>
2023-06-12 11:55:42 -0500

    added slider

--- ui/src/components/DataViewer/Graph/Graph.tsx
+++ ui/src/components/DataViewer/Graph/Graph.tsx
@@ -5,13 +5,24 @@ import styles from './Graph.module.css';
 import GraphKey from './GraphKey/GraphKey';
 import ElectronicChemicalPotentialInput from './ElectronicChemicalPotentialInput/ElectronicChemicalPotentialInput';
 // import PlotlyGraph from './PlotlyGraph/PlotlyGraph';
+import { ReactCompareSlider } from 'react-compare-slider';
+
+const CustomHandle = () => {
+    return <div style={{ backgroundColor: '#3747AC', width: '2px', height: '100%', cursor: 'ew-resize' }}></div>;
+};
 
 const Graph = () => {
     const [oxidationData] = useAtom(oxidationDataAtom);
 
     return (
         <div className={styles.container}>
-            {oxidationData && <CanvasGraph data={oxidationData} />}
+            {oxidationData && (
+                <ReactCompareSlider
+                    itemOne={<CanvasGraph data={oxidationData} />}
+                    itemTwo={<CanvasGraph data={oxidationData} />}
+                    handle={<CustomHandle />}
+                />
+            )}
             <ElectronicChemicalPotentialInput />
             <GraphKey />
         </div>
---
+++ ui/src/components/DataViewer/Graph/Slider/Slider.tsx
@@ -0,0 +1,9 @@
+//import { ReactCompareSlider } from 'react-compare-slider';
+
+const Slider = () => {
+    // return <ReactCompareSlider />;
+
+    return <></>;
+};
+
+export default Slider;
