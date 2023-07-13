e4735b59ed2c363d1b8fdbb4cde5855c9b1a470e
Danny <daniel.martinez@slalom.com>
2023-07-13 14:12:18 -0500

    added tooltip to graph headers and nudge buttons

--- ui/src/components/DataViewer/Graph/GraphHeaders/GraphHeaders.tsx
+++ ui/src/components/DataViewer/Graph/GraphHeaders/GraphHeaders.tsx
@@ -1,11 +1,16 @@
+import { Tooltip } from '@mui/material';
 import styles from './GraphHeaders.module.css';
 
 const GraphHeaders = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.headerText}>Species</div>
+            <Tooltip title={<span>The element or group of <br/> elements that are <br/> found in the ion.</span>}>
+                <div className={styles.headerText}>Species</div>
+            </Tooltip>
             <div className={styles.divider} />
+            <Tooltip title={<span>The electronic chemical <br /> potential ranges at which <br /> each oxidation state is likely <br/> to be observed</span>}>
             <div className={styles.headerText}>Oxidation States</div>
+            </Tooltip>
         </div>
     );
 };
