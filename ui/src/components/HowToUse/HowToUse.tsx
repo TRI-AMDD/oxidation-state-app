9fa10d79e3a7297344602f2f4aecee724119d01f
Danny <daniel.martinez@slalom.com>
2023-07-24 13:28:54 -0500

    added accordion to homepage

--- ui/src/components/HowToUse/HowToUse.tsx
+++ ui/src/components/HowToUse/HowToUse.tsx
@@ -1,22 +1,39 @@
-import { Typography } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
 import styles from './HowToUse.module.css';
 import LeftSide from './LeftSide';
 import RightSide from './RightSide';
 import CompareArrowsOutlinedIcon from '@mui/icons-material/CompareArrowsOutlined';
+import { useState } from 'react';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const HowToUse = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const handleAccordianChange = (event: React.SyntheticEvent, isExpanded: boolean) => {
+        setIsOpen(isExpanded);
+    };
     return (
         <div className={styles.container}>
-            <Typography component={'div'} variant="h6" className={styles.howToTitle}>
-                How to use our tool:
-            </Typography>
-            <div className={styles.howToCard}>
-                <LeftSide />
-                <div className={styles.compareIcon}>
-                    <CompareArrowsOutlinedIcon fontSize="large" />
-                </div>
-                <RightSide />
-            </div>
+            <Accordion
+                expanded={isOpen}
+                onChange={handleAccordianChange}
+                sx={{ background: 'none', boxShadow: 'none' }}
+            >
+                <AccordionSummary id="howToUse-header" expandIcon={<ExpandMoreIcon />}>
+                    <Typography component={'div'} variant="h6" className={styles.howToTitle}>
+                        How to use our tool:
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <div className={styles.howToCard}>
+                        <LeftSide />
+                        <div className={styles.compareIcon}>
+                            <CompareArrowsOutlinedIcon fontSize="large" />
+                        </div>
+                        <RightSide />
+                    </div>
+                </AccordionDetails>
+            </Accordion>
         </div>
     );
 };
