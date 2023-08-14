cd802b094e6dd45135492fc2fd2464641a722ebd
Michael Puzon <michael.puzon@tri.global>
2023-08-14 13:43:31 -0700

    Make MPV input box useable

--- ui/src/components/DataViewer/Graph/ElectronicChemicalPotentialInput/ElectronicChemicalPotentialInput.tsx
+++ ui/src/components/DataViewer/Graph/ElectronicChemicalPotentialInput/ElectronicChemicalPotentialInput.tsx
@@ -1,29 +1,53 @@
-import { TextField } from '@mui/material';
+import { TextField, TextFieldProps } from '@mui/material';
 import styles from './ElectronicChemicalPotentialInput.module.css';
 import { useAtom } from 'jotai';
 import { exportGraphModalOpenAtom } from '@/atoms/atoms';
+import { useDebouncedCallback } from 'use-debounce';
+import { useEffect, useRef } from 'react';
 
 interface ECPInputProps {
-    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    onChange: (value: number) => void;
     value: number;
 }
 
 const ElectronicChemicalPotentialInput = ({ onChange, value }: ECPInputProps) => {
-    const formattedValue = value.toFixed(4);
     const [isModalOpen] = useAtom(exportGraphModalOpenAtom);
+    const inputRef = useRef<TextFieldProps>();
+
+    useEffect(() => {
+        if (inputRef.current) {
+            const formattedValue = value.toFixed(5);
+            inputRef.current.value = formattedValue;
+        }
+    }, [value]);
+
+    // trigger value change after 1 sec
+    const debounced = useDebouncedCallback(
+        (val: string) => {
+            if (!Number.isNaN(Number.parseFloat(val))) {
+                const newValue = Number.parseFloat(val);
+                if (value !== newValue) {
+                    onChange(newValue);
+                }
+            }
+        },
+        // delay in ms
+        1000
+    );
+
+    if (isModalOpen) {
+        return null;
+    }
+
     return (
-        <>
-            {!isModalOpen && (
-                <TextField
-                    variant="filled"
-                    className={styles.textFieldContainer}
-                    label="Mapped Potential Value (E)"
-                    type="number"
-                    onChange={onChange}
-                    value={formattedValue}
-                />
-            )}
-        </>
+        <TextField
+            inputRef={inputRef}
+            variant="filled"
+            className={styles.textFieldContainer}
+            label="Mapped Potential Value (E)"
+            type="number"
+            onChange={(e) => debounced(e.target.value)}
+        />
     );
 };
 
