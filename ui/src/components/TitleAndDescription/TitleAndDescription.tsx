2b56a149d1a725139ebf4f67c8f995ff5ccd7259
Danny <daniel.martinez@slalom.com>
2023-09-05 10:37:15 -0500

    updated font; vertically aligned Get Started component

--- ui/src/components/TitleAndDescription/TitleAndDescription.tsx
+++ ui/src/components/TitleAndDescription/TitleAndDescription.tsx
@@ -7,14 +7,14 @@ const TitleAndDescription = () => {
             <Typography component={'div'} variant="h4" className={styles.title}>
                 Oxidation State Analyzer
             </Typography>
-            <Typography component={'div'} variant="body2">
+            <Typography component={'div'} variant="body1">
                 This tool can be used to quickly identify likely oxidation states for a given composition, or assign
                 likely
             </Typography>
-            <Typography component={'div'} variant="body2">
+            <Typography component={'div'} variant="body1">
                 oxidation states to sites in a given structure, using the methods described here.
             </Typography>
-            <Typography component={'div'} variant="body2" className={styles.subtext}>
+            <Typography component={'div'} variant="body1" className={styles.subtext}>
                 A library implementing the underlying methods is available to download for offline use
             </Typography>
         </div>
