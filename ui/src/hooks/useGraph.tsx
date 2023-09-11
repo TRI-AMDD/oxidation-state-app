9bd5f26f108fbf21e352c60936d06ca461f5a901
Michael Puzon <michael.puzon@tri.global>
2023-09-11 15:40:28 -0700

    TRI-28494 Fix slider on table row click

--- ui/src/hooks/useGraph.tsx
+++ ui/src/hooks/useGraph.tsx
@@ -1,21 +1,12 @@
 import { boundaryAtom, electronicChemicalPotentialRangeAtom, electronicMappedPotentialValueAtom } from '@/atoms/atoms';
 import { useAtom } from 'jotai';
-import { useEffect } from 'react';
-import useTable from './useTable';
 import { Boundary } from '@/models/DataViewerModel';
 
 const useGraph = () => {
     const [ECPValue, setECPValue] = useAtom(electronicMappedPotentialValueAtom);
     const [ECPRange] = useAtom(electronicChemicalPotentialRangeAtom);
     const [, setBoundary] = useAtom(boundaryAtom);
-    const { selectedRow } = useTable();
-
-    useEffect(() => {
-        if (selectedRow) {
-            setECPValue(selectedRow?.optimalMappedPotential);
-        }
-    }, [selectedRow, setECPValue]);
-
+    
     const handleMPVChange = (newECPValue: number) => {
         setECPValue(newECPValue);        
         setBoundary(null);
