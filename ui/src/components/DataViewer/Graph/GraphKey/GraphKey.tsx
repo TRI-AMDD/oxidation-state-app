56f347e02b383062f50fd368b41f51ba375b202e
Danny <daniel.martinez@slalom.com>
2023-09-20 11:11:58 -0500

    removed line at bottom when exporting

--- ui/src/components/DataViewer/Graph/GraphKey/GraphKey.tsx
+++ ui/src/components/DataViewer/Graph/GraphKey/GraphKey.tsx
@@ -1,16 +1,20 @@
 import { ReactComponent as ColorPalette } from '@/Assets/Images/graphKey.svg';
 import styles from './GraphKey.module.css';
 import { useAtom } from 'jotai';
-import { exportGraphSettingsAtom } from '@/atoms/atoms';
+import { exportGraphModalOpenAtom, exportGraphSettingsAtom } from '@/atoms/atoms';
 
 const GraphKey = () => {
     const [exportGraphSettings] = useAtom(exportGraphSettingsAtom);
+    const [isModalOpen] = useAtom(exportGraphModalOpenAtom);
     return (
         <>
             {exportGraphSettings.showColorKey && (
                 <div className={styles.container}>
-                    <div className={styles.keyText}>Key:</div>
-                    <ColorPalette />
+                    <div className={styles.innerContainer}>
+                        <div className={styles.keyText}>Key:</div>
+                        <ColorPalette />
+                    </div>
+                    {!isModalOpen && <div className={styles.divider} />}
                 </div>
             )}
         </>
