538c1a660aa749ac73ae9147921cea15837b15d9
mike puzon <michael.puzon@tri.global>
2023-11-01 16:24:26 -0700

    restore Home files

---
+++ ui/src/pages/Home/Home.tsx
@@ -0,0 +1,46 @@
+import styles from './Home.module.css';
+import { Grid } from '@mui/material';
+import TitleAndDescription from '@/components/TitleAndDescription/TitleAndDescription';
+import HowToUse from '@/components/HowToUse/HowToUse';
+import GetStarted from '@/components/GetStarted/GetStarted';
+import InputSection from '@/features/input-submission/input-section/input-section';
+import DataViewer from '@/components/DataViewer/DataViewer';
+import PageWrapper from '@/components/PageWrapper/PageWrapper';
+import { useEffect } from 'react';
+
+function Home() {
+    useEffect(() => {
+        document.title = 'Oxidation State Analyzer';
+    }, []);
+
+    return (
+        <PageWrapper>
+            <Grid
+                container
+                direction="column"
+                justifyContent="flex-start"
+                alignContent="center"
+                className={styles.AppMain}
+                spacing={2}
+                textAlign="center"
+            >
+                <Grid item className={styles.itemPadding}>
+                    <TitleAndDescription />
+                </Grid>
+                <Grid item className={styles.itemPadding}>
+                    <HowToUse />
+                </Grid>
+
+                <Grid item className={styles.itemPadding}>
+                    <GetStarted />
+                </Grid>
+                <Grid item className={styles.itemPadding}>
+                    <InputSection />
+                </Grid>
+            </Grid>
+            <DataViewer />
+        </PageWrapper>
+    );
+}
+
+export default Home;
